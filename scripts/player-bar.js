1144714fc8b2a8671c8316df263b5624baac2057
Yevgeniy Ivanov <anigeka@gmail.com>
2018-01-06 19:56:12 -0600

    need help

---
+++ scripts/player-bar.js
@@ -0,0 +1,30 @@
+{
+  $('button#play-pause').on('click', function(){
+    player.playPause();
+    $(this).attr('playState', player.playState);
+  });
+
+  $('button#next').on('click', function(){
+    if(player.playState !== 'playing') {return;}
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const nextSongIndex = currentSongIndex + 1;
+    if(nextSongIndex >= album.songs.length) {return;}
+
+    const nextSong = album.songs[nextSongIndex];
+    player.playPause(nextSong);
+  });
+
+  $('button#previous').on('click', function(){
+    if(player.playState !== 'playing') {return;}
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    if(currentSongIndex === 0) {return;}
+    else if(currentSongIndex > 0) {
+    const previousSongIndex = currentSongIndex - 1;
+  }
+
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
+  });
+}
