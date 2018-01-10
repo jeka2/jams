1d42f27b7dddaf71affa003944e66244d4cbd0e6
Yevgeniy Ivanov <anigeka@gmail.com>
2018-01-10 15:54:51 -0600

    finished checkpoint 16

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,35 +1,36 @@
-{
-  $('button#play-pause').on('click', function(){
-    player.playPause();
-    $(this).attr('playState', player.playState);
-  });
-
-  $('button#next').on('click', function(){
-    if(player.playState !== 'playing') {return;}
-
-    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const nextSongIndex = currentSongIndex + 1;
-    if(nextSongIndex >= album.songs.length) {return;}
-
-    const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
-  });
-
-  $('button#previous').on('click', function(){
-    if(player.playState !== 'playing') {return;}
-    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    if(currentSongIndex === 0) {return;}
-    const previousSongIndex = currentSongIndex - 1;
-
-    const previousSong = album.songs[previousSongIndex];
-    player.playPause(previousSong);
-  });
-
-  setInterval( () => {
-  if (player.playState !== 'playing') {return;}
-  const currentTime = player.getTime();
-  const duration = player.getDuration();
-  const percent = (currentTime / duration) * 100;
-  $('#time-control input').val(percent);
-  }, 1000);
-}
+// {
+//   $('button#play-pause').on('click', function(){
+//     player.playPause();
+//     $(this).attr('playState', player.playState);
+//   });
+//
+//   $('button#next').on('click', function(){
+//     if(player.playState !== 'playing') {return;}
+//
+//     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+//     const nextSongIndex = currentSongIndex + 1;
+//     if(nextSongIndex >= album.songs.length) {return;}
+//
+//     const nextSong = album.songs[nextSongIndex];
+//     player.playPause(nextSong);
+//   });
+//
+//   $('button#previous').on('click', function(){
+//     if(player.playState !== 'playing') {return;}
+//
+//     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+//     if(currentSongIndex === 0) {return;}
+//     const previousSongIndex = currentSongIndex - 1;
+//
+//     const previousSong = album.songs[previousSongIndex];
+//     player.playPause(previousSong);
+//   });
+//
+//   setInterval( () => {
+// if (player.playState !== 'playing') {return;}
+// const currentTime = player.getTime();
+// const duration = player.getDuration();
+// const percent = (currentTime / duration) * 100;
+// $('#time-control input').val(percent);
+// }, 1000);
+// }
